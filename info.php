da6a7bb69f00c4c8b0443ce07562ccb5c9d3bdd8
Bony_Chops <office.kentarou@gmail.com>
2020-04-28 16:32:06 +0900

    Init

---
+++ info.php
@@ -0,0 +1,45 @@
+<?php
+$ipAddress=$_SERVER['REMOTE_ADDR'];
+$macAddr=false;
+
+function getMac($ip){
+    echo PHP_OS;
+
+    $pramPos = [[0,1],[2,4]];
+
+    $WIN = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? true : false;
+
+    $arp=exec('arp -a '.$ip);
+    if($arp == "") return false;
+    $arp = mb_convert_encoding($arp, "UTF-8", "SJIS");
+    var_dump($arp);
+    $lines=explode("\n", $arp);
+    #look for the output line describing our IP address
+    if((strpos($arp, 'ARP エントリが見つかりませんでした。') === FALSE)&&(strpos($arp, '無効な引数') === FALSE)){
+        foreach($lines as $line)
+        {
+            $cols = uExplodeSpace($line);
+            var_dump($cols);
+            $ipPos = $WIN == true ? $cols[$pramPos[0][0]] : sscanf($cols[$pramPos[1][0]],"($s)")[0];
+            if($ipPos == $ip) {
+                $macAddr = $WIN == true ? $cols[$pramPos[0][1]] : $cols[$pramPos[1][1]];
+                //break;
+            }
+        }
+        return $macAddr;
+    }else{
+        return false;
+    }
+}
+
+getMac($ipAddress);
+
+var_dump($macAddr);
+
+function uExplodeSpace($data) {
+    $data = preg_replace('/[\n\r\t]/', ' ', $data);
+    $data = preg_replace('/\s(?=\s)/', '', $data); // 複数スペースを一つへ
+    $data = trim($data);
+    //echo "data=" . htmlspecialchars($data) . "<br>\n";
+    return explode(" ", $data);
+}
---
+++ test.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Starter Template - Materialize</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img src="https://bonychops.com/favicon.ico" width="12%">Bony_LAB</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">Welcome to Bony_LAB!</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">ネットワークを利用するには、Bony_AUTHによる認証が必要です。</h5>
+      </div>
+      <div class="row center">
+        <a href="vscode://" id="download-button" class="btn-large waves-effect waves-light orange">LOGIN</a>
+      </div>
+      <div class="row center">
+        Bony_AUTHをPCにインストールする必要があります。<br><a href="./">Bony_AUTHをPCにインストール</a>
+      </div>
+
+      <br><br>
+
+    </div>
+  </div>
+
+
+  
+
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
