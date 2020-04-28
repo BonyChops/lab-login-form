fcb459172f65ab008094aebfb112e0bd44dbd132
Bony_Chops <office.kentarou@gmail.com>
2020-04-28 16:58:59 +0900

    Edit

--- info.php
+++ info.php
@@ -21,7 +21,7 @@ function getMac($ip){
             $cols = uExplodeSpace($line);
             var_dump($cols);
             $ipPos = $WIN == true ? $cols[$pramPos[0][0]] : substr(sscanf($cols[$pramPos[1][0]],"($s)")[0], 0, -1);
-            echo $cols[$pramPos[1][0]],"($s)";
+            echo sscanf($cols[$pramPos[1][0]],"($s)")[0];
             if($ipPos == $ip) {
                 $macAddr = $WIN == true ? $cols[$pramPos[0][1]] : $cols[$pramPos[1][1]];
                 break;
